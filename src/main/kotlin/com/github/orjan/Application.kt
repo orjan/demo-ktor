79cafb25da0a61b7ba3acf16f47f81909aee623a
Örjan Sjöholm <orjansjoholm@gmail.com>
2017-12-01 10:07:15 +0100

    Hello world

--- src/main/kotlin/com/github/orjan/Application.kt
+++ src/main/kotlin/com/github/orjan/Application.kt
@@ -1,5 +1,20 @@
 package com.github.orjan
 
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
 fun main(args: Array<String>) {
-    println("Hello world!")
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
 }
